72c17c5bdca3015639dfb005dd097fbb94a7526f
suriyaravi20 <suriyaravi20@gmail.com>
2023-05-10 07:49:46 +0530

    initial commit

---
+++ testJenkins/src/testJenkins/JenkinsTest.java
@@ -0,0 +1,11 @@
+package testJenkins;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Jenkins uploaded file");
+	}
+
+}
